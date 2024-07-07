cd8d46f4a33342ab618363bbafa8ec554cbf40a1
internethero401 <ugoku30@gmail.com>
2024-07-07 11:36:30 +0900

    testtest

--- main/2.py
+++ main/2.py
@@ -1,10 +1,32 @@
+import socket
+
+TELLO_IP = '192.168.10.1'
+TELLO_PORT = 8889
+TELLO_ADDRESS = (TELLO_IP, TELLO_PORT)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', 9000))
+
+
 def send(message):
     try:
         sock.sendto(message.encode(), TELLO_ADDRESS)
         print(f"Sending message: {message}")
     except Exception as e:
         print(f"Error sending message: {e}")
-        
-distance = 100
+
+send("command")
+receive()
+
+send("takeoff")
+time.sleep(5)
+
+distance = 50
 send(f"up {distance}")
-time.sleep(8)
+time.sleep(5)
+
+send("land")
+receive()
+
+# ソケットを閉じる
+sock.close()
