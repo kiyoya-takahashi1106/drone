17ab2a89095edf0d8635f43ae244229d7da93704
internethero401 <ugoku30@gmail.com>
2024-07-09 18:11:13 +0900

    7/09

--- drone_app/src/App.js
+++ drone_app/src/App.js
@@ -1,24 +1,64 @@
 import React from 'react';
+import TopHeader from './Components/TopHeader';
+import LeftHeader from './Components/LeftHeader';
+import { useState } from 'react';
 
 function App() {
+  const [N, setN] = useState(null)
+  const [M, setM] = useState(null)
+  const [height, setHeight] = useState(null)
+  const [width, setWidth] = useState(null)
+  const handleSetN = (event) => {
+    setN(event.target.value);
+  }
+  const handleSetM = (event) => {
+    setM(event.target.value);
+  }
+  const handleSetHeight = (event) => {
+    setHeight(event.target.value);
+  }
+  const handleSetWidth = (event) => {
+    setWidth(event.target.value);
+  }
+
   return (
-    <div style={{
-      height: '840px',
-      width: '390px',
-      display: 'flex',
-      justifyContent: 'center', // 横方向の中央に配置
-      alignItems: 'center', // 縦方向の中央に配置
-      background: 'white'
-    }}>
-      <div style={{
-        textAlign: 'center',
-        display: 'flex',
-        flexDirection: 'column', // 子要素を縦並びに
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-        <div style={{ fontSize: '20px' }}>auto</div>
-        <div style={{ fontSize: '30px' }}>Tello</div>
+    <div style={{ height: '832px', width: '1280px' }}>
+      <TopHeader />
+      <div style={{ display: 'flex' }}>
+        <LeftHeader />
+        <div style={{ height: '678px', width: '944px' }}>
+          <div style={{ height: '471px', width: '944px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <div style={{ height: '431px', width: '815px', marginTop: '20px', borderRadius: '50px', backgroundColor: '#D9D9D9' }}>
+            </div>
+          </div>
+          <div style={{ height: '206px', width: '944px', display: 'flex' }}>
+            <div style={{ height: '206px', width: '708px', fontSize: '36px', fontFamily: '"Abel", sans-serif' }}>
+              <div style={{ marginTop: '45px', marginLeft: '45px', display: 'flex' }}>
+                <div style={{ marginRight: '50px' }}>個数:</div>
+                <div>N(縦) =</div>
+                <input type="number" value={N} onChange={handleSetN} style={{ height: '60px', width: '100px', fontSize: '40px', textAlign: 'center', backgroundColor: '#D9D9D9', border: 'none', borderRadius: '30px', cursor: 'pointer' }}></input>
+                <div style={{ marginRight: '40px' }}>,</div>
+                <div>M(横) =</div>
+                <input type="number" value={M} onChange={handleSetM} style={{ height: '60px', width: '100px', fontSize: '40px', textAlign: 'center', backgroundColor: '#D9D9D9', border: 'none', borderRadius: '30px', cursor: 'pointer' }}></input>
+              </div>
+              <div style={{ marginTop: '10px', marginLeft: '45px', display: 'flex' }}>
+                <div style={{ marginRight: '46px' }}>長さ:</div>
+                <div>height =</div>
+                <input type="number" value={height} onChange={handleSetHeight} style={{ height: '60px', width: '100px', fontSize: '40px', textAlign: 'center', backgroundColor: '#D9D9D9', border: 'none', borderRadius: '30px', cursor: 'pointer' }}></input>
+                <div style={{ marginRight: '45px' }}>,</div>
+                <div>width =</div>
+                <input type="number" value={width} onChange={handleSetWidth} style={{ height: '60px', width: '100px', fontSize: '40px', textAlign: 'center', backgroundColor: '#D9D9D9', border: 'none', borderRadius: '30px', cursor: 'pointer' }}></input>
+              </div>
+            </div>
+
+            <div style={{ height: '206px', width: '236px', fontFamily: '"Zen Dots", sans-serif', display: 'flex', alignItems: 'center' }}>
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <button style={{ height: '70px', width: '180px', fontSize: '40px', fontFamily: '"Zen Dots", sans-serif', backgroundColor: '#D9D9D9', marginTop: '70px', border: 'none', cursor: 'pointer' }}>Next</button>
+                <button style={{ border: 'none', fontSize: '30px', fontFamily: '"Zen Dots", sans-serif', backgroundColor: 'white', marginTop: '12px', cursor: 'pointer' }}>Save</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ drone_app/src/Components/LeftHeader.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const LeftHeader = () => {
+  return (
+    <div style={{ height: '683px', width: '335.48px', backgroundColor: 'rgba(195, 251, 255, 0.5)', fontFamily: '"Zen Dots", sans-serif', borderRight: '2px solid black', display: 'flex', flexDirection: 'column' }}>
+      <div style={{ fontSize: '40px', marginTop: '20px', display: 'flex', alignItems: 'center', marginLeft: '104px' }}>menu</div>
+      {/*
+      <div style={{ fontSize: '18px', marginTop: '40px'  }}>known_face</div>
+      <div style={{ fontSize: '18px', marginTop: '30px' }}>registered_room</div>
+      <div style={{ fontSize: '18px', marginTop: '30px' }}>how_to_use</div>
+      <div style={{ fontSize: '18px', marginTop: '30px' }}>picture</div>
+      */}
+    </div>
+  );
+};
+
+export default LeftHeader;
