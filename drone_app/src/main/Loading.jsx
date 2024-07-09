b99608003423512fcfc193f44b3e9555fc19e88c
internethero401 <ugoku30@gmail.com>
2024-07-10 00:58:47 +0900

    7/10

--- drone_app/src/main/Loading.jsx
+++ drone_app/src/main/Loading.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Loading = () => {
   return (
-    <div style={{ height: '832px', width: '1280px', backgroundColor: '#9A9A9A', position: 'relative', border: 'none', display: 'flex' }}>
-        <div style={{ width: '100%', height: '100%', backgroundColor: '#D45454', clipPath: 'polygon(3% 0%, 58.5% 0%, 38.5% 100%, 3% 100%)', position: 'absolute' }}></div>
-        <div style={{ width: '100%', height: '100%', backgroundColor: '#4DB5E1', clipPath: 'polygon(61.5% 0%, 97% 0%, 97% 100%, 41.5% 100%)', position: 'absolute' }}></div>
-        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', fontFamily: '"Zen Dots", sans-serif', color: 'white', textShadow: '3px 3px 0 black, -3px -3px 0 black, -3px 3px 0 black, 3px -3px 0 black, 2px 2px 0 black, -2px -2px 0 black, -2px 2px 0 black, 2px -2px 0 black, 1px 1px 0 black, -1px -1px 0 black, -1px 1px 0 black, 1px -1px 0 black' , display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
-            <div style={{fontSize: '40px', margin: '0px 100px 0px 0px' }}>auto</div>
-            <div style={{fontSize: '70px', margin: '-20px 0px 0px 0px' }}>Tello</div>
+    <Link to="/home">
+        <div style={{ height: '832px', width: '1280px', backgroundColor: '#9A9A9A', position: 'relative', border: 'none', display: 'flex' }}>
+            <div style={{ width: '100%', height: '100%', backgroundColor: '#D45454', clipPath: 'polygon(3% 0%, 58.5% 0%, 38.5% 100%, 3% 100%)', position: 'absolute' }}></div>
+            <div style={{ width: '100%', height: '100%', backgroundColor: '#4DB5E1', clipPath: 'polygon(61.5% 0%, 97% 0%, 97% 100%, 41.5% 100%)', position: 'absolute' }}></div>
+            <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', fontFamily: '"Zen Dots", sans-serif', color: 'white', textShadow: '3px 3px 0 black, -3px -3px 0 black, -3px 3px 0 black, 3px -3px 0 black, 2px 2px 0 black, -2px -2px 0 black, -2px 2px 0 black, 2px -2px 0 black, 1px 1px 0 black, -1px -1px 0 black, -1px 1px 0 black, 1px -1px 0 black' , display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+                <div style={{fontSize: '40px', margin: '0px 100px 0px 0px' }}>auto</div>
+                <div style={{fontSize: '70px', margin: '-20px 0px 0px 0px' }}>Tello</div>
+            </div>
         </div>
-    </div>
+    </Link>
   );
 };
 
 export default Loading
-
--- drone_app/src/main/RegisteredRoom.jsx
+++ drone_app/src/main/RegisteredRoom.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+// import { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import Desk from '../Components/Desk';
+import TopHeader from '../Components/TopHeader';
+import LeftHeader from '../Components/LeftHeader';
 
-const RegisteredRoom = () => {
+function RegisteredRoom() {
   return (
-    <div>RegisteredRoom</div>
-  )
+    <div style={{ height: '832px', width: '1280px' }}>
+      <TopHeader s={"Registered Room"} />
+      <div style={{ display: 'flex' }}>
+        <LeftHeader />
+        <div style={{ height: '678px', width: '944px' }}>
+          <div style={{ height: '471px', width: '944px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default RegisteredRoom
+export default RegisteredRoom;
