8ac1f3a383b36f1e4c9a8e04d1892d474035cf70
internethero401 <ugoku30@gmail.com>
2024-07-10 18:51:11 +0900

    -Room.jsxを作成し,登録した部屋からTelloSettingに飛べるようにしまし. TelloSettingからWifiConnect.jsxも飛べるようにして いずれもurlにN,M,height,width指定しパラメーターをurlに格納してます.

---
+++ drone_app/src/Components/Room.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import startIcon from '../img/startIcon.jpg'
+import deleteIcon from '../img/deleteIcon.png'
+import { Link } from 'react-router-dom'
+
+
+const Room = ({ roomInformation }) => {
+  return (
+    <div style={{ height: '50px', width: '800px', margin: '5px 0px 0px 0px', display: 'flex', alignItems: 'center', border: '2px solid black', fontFamily: '"Abel", sans-serif' }}>
+        <div style={{ height: '50px', width: '700px', fontSize: '27px', display: 'flex', alignItems: 'center' }}>
+            <div style={{ margin: '0px 10px 0px 20px' }}>部屋名:{roomInformation.roomName},</div>
+            <div style={{ margin: '0px 10px 0px 0px' }}>N:{roomInformation.N},</div>
+            <div style={{ margin: '0px 10px 0px 0px' }}>M:{roomInformation.M},</div>
+            <div style={{ margin: '0px 10px 0px 0px' }}>height:{roomInformation.height},</div>
+            <div style={{ margin: '0px 10px 0px 0px' }}>width:{roomInformation.width}</div>
+        </div>
+        <Link to={{ pathname: "/home/setting/"+roomInformation.N+"/"+roomInformation.M+"/"+roomInformation.height+"/"+roomInformation.width }}>
+            <button style={{ height: '48px', width: '50px', backgroundImage: `url(${startIcon})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', border: 'none', cursor: 'pointer' }}></button>
+            <button style={{ height: '48px', width: '50px', backgroundImage: `url(${deleteIcon})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', border: 'none', cursor: 'pointer' }}></button>
+        </Link>
+    </div>
+  )
+}
+
+export default Room
